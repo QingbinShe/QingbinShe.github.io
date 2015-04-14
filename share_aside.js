2524d3205f99b1a9e3c7d4ca33ea2514d5a2b7ba
sheqingbin <sheqingbin@gmail.com>
2015-04-14 22:34:59 +0800

    full

---
+++ share_aside.js
@@ -0,0 +1,12 @@
+document.write("<font size='5'>Index:</font>");
+document.write("<li><a href='./index.html'><font size='3'>Index</font></a></li>");
+document.write("<li><font size='3'>....</font></li>");
+document.write("<hr />");
+
+document.write("<font size='5'>Software:</font>");
+document.write("<li><a href='./install_jawstats.html'><font size='3'>awstats + jawstats</font></a></li>");
+document.write("<li><font size='3'>....</font></li>");
+document.write("<hr />");
+
+document.write("<font size='5'>Others:</font>")
+document.write("<li><font size='3'>....</font></li>");
