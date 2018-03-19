642d36deaa03822053479dd3be7f2d1acfcf8efb
QingbinShe <sheqingbin@gmail.com>
2018-03-19 14:04:20 +0800

    test
    test

--- share_aside.js
+++ share_aside.js
@@ -9,5 +9,10 @@ document.write("<li><a href='./install_jawstats_ch.html'><font size='3'>awstats 
 document.write("<li><font size='3'>....</font></li>");
 document.write("<hr />");
 
+document.write("<font size='5'>DataBase:</font>");
+document.write("<li><a href='./index.html'><font size='3'>Index</font></a></li>");
+document.write("<li><font size='3'>....</font></li>");
+document.write("<hr />");
+
 document.write("<font size='5'>Others:</font>")
 document.write("<li><font size='3'>....</font></li>");
